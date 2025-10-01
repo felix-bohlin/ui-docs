fe3520879c972a22ded01879c66120cd41ddb9a0
Felix <felix.bohlin@protonmail.com>
2025-10-01 10:26:05 +0200

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/ui-docs/" : "/",
   ssr: {
     noExternal: ["baseline-status"],
   },
